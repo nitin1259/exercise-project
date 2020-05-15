56aeecc5eb2e524fe10776de1db5ff11fc2ebacc
Nitin SINGH <nitinkumar.singh@f5.com>
2020-05-15 22:34:40 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,41 +1,32 @@
 "use strict";
 
-// const arr = [3, 2, 1, 2, 4, 5, 3];
+const arr = [3, 2, 1, 2, 4, 5, 3];
 
-// let obj = {};
-// let res = [];
+let obj = {};
+let res = [];
 
-// console.log(typeof obj);
-// console.log(typeof res);
+console.log(typeof obj);
+console.log(typeof res);
 
-// for (let i = 0; i < arr.length; i++) {
-//   let item = arr[i];
-//   if (!obj[item]) obj[item] = 0;
-//   obj[item] = obj[item] + 1;
-// }
+for (let i = 0; i < arr.length; i++) {
+  let item = arr[i];
+  if (!obj[item]) obj[item] = 0;
+  obj[item] = obj[item] + 1;
+}
 
-// for (var prop in obj) {
-//   if (obj[prop] > 1) {
-//     res.push(prop);
-//   }
-// }
+for (var prop in obj) {
+  console.log(prop, obj[prop]);
+  if (obj[prop] > 1) {
+    res.push(prop);
+  }
+}
 
-// console.log(res);
+console.log(res);
 
-// function dostuf(a) {
-//   return function (b) {
-//     return function (c) {
-//       return a * b * c;
-//     };
-//   };
-// }
-
-console.log(age);
-
-someFunc();
-
-var age = 10;
-
-function someFunc() {
-  console.log("some func");
+function dostuf(a) {
+  return function (b) {
+    return function (c) {
+      return a * b * c;
+    };
+  };
 }
