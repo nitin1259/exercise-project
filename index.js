aa6714b3c60579c730dd423651a7633ba635aa0c
Nitin Singh <nitin1259@users.noreply.github.com>
2020-07-22 23:39:48 +0530

    Update index.js
    ciena UI test

--- index.js
+++ index.js
@@ -3,3 +3,56 @@
 // custom hooks
 // multiple api call in parallel.
 // performance testing in web application.
+
+
+// ciena
+
+
+// 1. <div class='c-red c-green'>Hello</div>
+// 2. <div class='c-green c-red'>Hello</div>
+// // Stylesheet
+// .c-red {color: red}
+// div .c-green {color: green}
+
+
+// wrapInDispatch(actions, dispatch) -- wrappedActions
+// actions = {
+//   action1: (x1, x2) => actionCreator1(x1, x2),
+//   action2: (x1) => actionCreator2(x1)
+// }
+// function wrapInDispatch(actions, dispatch) {
+//   let wrappedActions = {}
+  
+//   arr = actions.getkeys.forEach(act =>{
+    
+//     return dispatch(actions[act])
+//   })
+  
+  
+//   return wrappedActions
+// }
+// wrappedActions = {
+//   action1: (x1, x2) => dispatch(actionCreator1(x1, x2)),
+//   action2: (x1) => dispatch(actionCreator2(x1))
+// }
+
+
+// HOC
+// LazyloadMe
+// ->path -- String
+// ->otherProps -- Object
+// import
+// ->Static -- import X from 'path'
+// ->Dynamic -- import('path') -->Promise -> X
+
+// const LazyLoad = (path, obj) =>{
+    
+//     return new Promise( resolve =>{
+//       const path = require(path)
+      
+      
+      
+//     }, reject =>{
+      
+//     })
+// }
