1cd3822f45cebc18dcf36dc038673bd88f7dd3a3
Nitin SINGH <nitinkumar.singh@f5.com>
2020-07-29 15:36:03 +0530

    go encapsulation example

--- go-exaples/main.go
+++ go-exaples/main.go
@@ -2,35 +2,18 @@ package main
 
 import "fmt"
 
-// Human interface for ploymorphism
-type Human interface {
-	doSomething()
-}
-
 // Person struct for holding person details
-type Person struct {
+type Person struct { //public
 	name   string
 	age    int
 	salary float64
 }
 
-func (p Person) doSomething() {
-	fmt.Println("doing something")
+func (p Person) raiseSalary(bonus float64) { //private method
+	p.salary += bonus
 }
 
-func main() {
-
-	var p1 Human
-	// create struct specifying field name
-	p1 = Person{
-		name:   "TechoNeo",
-		age:    22,
-		salary: 20000,
-	}
-
-	// create struct without specify struct name.
-	p2 := Person{"Techo-Neo", 23, 45000}
-
-	fmt.Println("Person 1", p1)
-	fmt.Println("Person 2", p2)
+// PrintInFormat - format printing
+func (p Person) PrintInFormat() string { // public method
+	return fmt.Sprintf("Name: %s, Age: %d, Salary: %.2f", p.name, p.age, p.salary)
 }
